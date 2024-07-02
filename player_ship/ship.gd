41f9aa1de914e48812ed2b8fc3ec855dec8635e9
Romualdo "Romuboy" Santos <romualdoosfilho94@gmail.com>
2024-07-02 16:23:58 -0300

    ship animation

--- player_ship/ship.gd
+++ player_ship/ship.gd
@@ -5,12 +5,30 @@ extends Node2D
 @onready var spawner_component: SpawnerComponent = $SpawnerComponent as SpawnerComponent
 @onready var fire_rate_timer = $FireRateTimer
 @onready var scale_component: ScaleComponent = $ScaleComponent as ScaleComponent
+@onready var animated_sprite_2d = $Anchor/AnimatedSprite2D
+@onready var move_component : MoveComponent = $MoveComponent as MoveComponent
+@onready var flame_animated_sprite = $Anchor/FlameAnimatedSprite
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	fire_rate_timer.timeout.connect(fire_laser)
 
-func fire_laser():
+func fire_laser() -> void:
 	spawner_component.spawn(left_muzzle.global_position)
 	spawner_component.spawn(right_muzzle.global_position)
 	scale_component.tween_scale()
+
+func _process(delta) -> void:
+	animate_the_ship()
+
+func animate_the_ship():
+	if move_component.velocity.x < 0:
+		animated_sprite_2d.play("bank_left")
+		flame_animated_sprite.play("bank_left")
+	elif move_component.velocity.x > 0:
+		animated_sprite_2d.play("bank_right")
+		flame_animated_sprite.play("bank_right")
+	else:
+		animated_sprite_2d.play("center")
+		flame_animated_sprite.play("center")
+	
