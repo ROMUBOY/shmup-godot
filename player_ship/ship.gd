b809e622123ea92cbabbc1ca00e102942736584e
Romualdo "Romuboy" Santos <romualdoosfilho94@gmail.com>
2024-07-03 07:48:13 -0300

    explosion effect

--- player_ship/ship.gd
+++ player_ship/ship.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @onready var left_muzzle = $LeftMuzzle
 @onready var right_muzzle = $RightMuzzle
-@onready var spawner_component: SpawnerComponent = $SpawnerComponent as SpawnerComponent
+@onready var laser_spawner_component: SpawnerComponent = $LaserSpawnerComponent as SpawnerComponent
 @onready var fire_rate_timer = $FireRateTimer
 @onready var scale_component: ScaleComponent = $ScaleComponent as ScaleComponent
 @onready var animated_sprite_2d = $Anchor/AnimatedSprite2D
@@ -14,8 +14,8 @@ func _ready() -> void:
 	fire_rate_timer.timeout.connect(fire_laser)
 
 func fire_laser() -> void:
-	spawner_component.spawn(left_muzzle.global_position)
-	spawner_component.spawn(right_muzzle.global_position)
+	laser_spawner_component.spawn(left_muzzle.global_position)
+	laser_spawner_component.spawn(right_muzzle.global_position)
 	scale_component.tween_scale()
 
 func _process(delta) -> void:
