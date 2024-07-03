5ebd362831eb1fa88ddfc20b2c0de9dad94aeaee
Romualdo "Romuboy" Santos <romualdoosfilho94@gmail.com>
2024-07-03 20:29:08 -0300

    yellow enemy

--- enemies/EnemyGenerator.gd
+++ enemies/EnemyGenerator.gd
@@ -1,16 +1,19 @@
 extends Node2D
 
 @export var GreenEnemyScene: PackedScene
+@export var YellowEnemyScene: PackedScene
 
 var margin = 8
 var screen_widith = ProjectSettings.get_setting("display/window/size/viewport_width")
 
 @onready var spawner_component = $SpawnerComponent
 @onready var green_enemy_spawner_timer = $GreenEnemySpawnerTimer
+@onready var yellow_enemy_spawner_timer = $YellowEnemySpawnerTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	green_enemy_spawner_timer.timeout.connect(handle_spawn.bind(GreenEnemyScene, green_enemy_spawner_timer))
+	yellow_enemy_spawner_timer.timeout.connect(handle_spawn.bind(YellowEnemyScene, yellow_enemy_spawner_timer))
 
 func handle_spawn(enemy_scene : PackedScene, timer: Timer) -> void:
 	spawner_component.scene = enemy_scene
