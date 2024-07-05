dccbdb6cb5f5604ebe1a0b4c123c18b87263e673
Romualdo "Romuboy" Santos <romualdoosfilho94@gmail.com>
2024-07-05 18:07:23 -0300

    pink enemy

--- enemies/EnemyGenerator.gd
+++ enemies/EnemyGenerator.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @export var GreenEnemyScene: PackedScene
 @export var YellowEnemyScene: PackedScene
+@export var PinkEnemyScene: PackedScene
 
 var margin = 8
 var screen_widith = ProjectSettings.get_setting("display/window/size/viewport_width")
@@ -9,11 +10,13 @@ var screen_widith = ProjectSettings.get_setting("display/window/size/viewport_wi
 @onready var spawner_component = $SpawnerComponent
 @onready var green_enemy_spawner_timer = $GreenEnemySpawnerTimer
 @onready var yellow_enemy_spawner_timer = $YellowEnemySpawnerTimer
+@onready var pink_enemy_spawner_timer = $PinkEnemySpawnerTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	green_enemy_spawner_timer.timeout.connect(handle_spawn.bind(GreenEnemyScene, green_enemy_spawner_timer))
 	yellow_enemy_spawner_timer.timeout.connect(handle_spawn.bind(YellowEnemyScene, yellow_enemy_spawner_timer))
+	pink_enemy_spawner_timer.timeout.connect(handle_spawn.bind(PinkEnemyScene, pink_enemy_spawner_timer))
 
 func handle_spawn(enemy_scene : PackedScene, timer: Timer) -> void:
 	spawner_component.scene = enemy_scene
---
+++ enemies/pink_enemy.gd
@@ -0,0 +1,33 @@
+extends Enemy
+
+@onready var states = %States
+@onready var move_down_state = %MoveDownState as TimedStateComponent
+@onready var move_side_state = %MoveSideState as TimedStateComponent
+@onready var pause_state = %PauseState as TimedStateComponent
+@onready var fire_state = %FireState as StateComponent
+@onready var move_side_component = %MoveSideComponent as MoveComponent
+@onready var projectile_spawner_component = %ProjectileSpawnerComponent
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	super()
+	
+	for state in states.get_children():
+		state = state as StateComponent
+		state.disable()
+	
+	move_side_component.velocity.x = [-20, 20].pick_random()
+	
+	move_down_state.state_finished.connect(move_side_state.enable)
+	move_side_state.state_finished.connect(func():
+		fire_state.enable()
+		scale_component.tween_scale()
+		projectile_spawner_component.spawn(global_position)
+		fire_state.disable()
+		fire_state.state_finished.emit()
+	)
+	fire_state.state_finished.connect(pause_state.enable)
+	pause_state.state_finished.connect(move_down_state.enable)
+	
+	move_down_state.enable()
